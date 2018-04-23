92d5b79c16815dbf5d043af224b819d488b3b7c8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to perform case sensitive filtering
+
+
+<p><br />
+The build with the capability to perform case sensitive filtering is not yet implemented. Please track our progress with the suggestion: <a href="https://www.devexpress.com/Support/Center/p/S37139">Allow case sensitive filtering in DXGrid</a><br />
+At present, it is possible to use the GridControl.CustomRowFilter event to achieve this functionality.</p>
+
+<br/>
+
+
---
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -0,0 +1,87 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports System.Collections.ObjectModel
+Imports DevExpress.Data.Filtering.Helpers
+Imports System.ComponentModel
+Imports DevExpress.Data.Filtering
+
+Namespace WpfApplication1
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			Dim coll As New ObservableCollection(Of MyObj)()
+			coll.Add(New MyObj() With {.Text = "A", .Number = 1, .Group = "A"})
+			coll.Add(New MyObj() With {.Text = "a", .Number = 2, .Group = "A"})
+			coll.Add(New MyObj() With {.Text = "a", .Number = 3, .Group = "B"})
+			coll.Add(New MyObj() With {.Text = "B", .Number = 4, .Group = "B"})
+			gridControl1.ItemsSource = coll
+		End Sub
+
+		Private Sub gridControl1_CustomRowFilter(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.RowFilterEventArgs)
+			Dim row = gridControl1.GetRowByListIndex(e.ListSourceRowIndex)
+			Dim ee As New ExpressionEvaluator(TypeDescriptor.GetProperties(row), gridControl1.FilterCriteria,If(checkEdit1.IsChecked = True, True, False))
+			Dim obj As Object = ee.Evaluate(row)
+
+			If obj IsNot Nothing Then
+				e.Visible = Convert.ToBoolean(obj)
+			End If
+
+			e.Handled = True
+
+		End Sub
+
+
+		Private Sub checkEdit1_EditValueChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Editors.EditValueChangedEventArgs)
+			gridControl1.RefreshData()
+			'CriteriaOperator co = gridControl1.FilterCriteria;
+			'gridControl1.FilterCriteria = null;
+			'gridControl1.FilterCriteria = co;
+		End Sub
+	End Class
+	Public Class MyObj
+		Private _String As String
+		Public Property Text() As String
+			Get
+				Return _String
+			End Get
+			Set(ByVal value As String)
+				_String = value
+			End Set
+		End Property
+		Private _Number As Double
+		Public Property Number() As Double
+			Get
+				Return _Number
+			End Get
+			Set(ByVal value As Double)
+				_Number = value
+			End Set
+		End Property
+		Private _Group As String
+		Public Property Group() As String
+			Get
+				Return _Group
+			End Get
+			Set(ByVal value As String)
+				_Group = value
+			End Set
+		End Property
+
+	End Class
+End Namespace
